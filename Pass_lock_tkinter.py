94c1a445c1e01125481698bacf779801475f8e85
Sumedh-smd <90448315+Sumedh-smd@users.noreply.github.com>
2022-01-07 19:37:20 +0530

    Modified

--- Pass_lock_tkinter.py
+++ Pass_lock_tkinter.py
@@ -1,18 +1,37 @@
+from tkinter import *
+
+window = Tk()
+window.title('Password lock')
+window.geometry("320x200")
+
 password = 1729
 
-chances = 4
+input_label = Label(window, text=" Enter Pin :", font=("Arial ", 17))
+input_label.grid(column=0, row=0)
 
-print("Enter Pin : ")
-while chances >= 0:
-    pin = int(input(" > "))
+input_pass = Entry(window, width=14)
+input_pass.grid(column=1, row=0)
+
+alert_label = Label(window, text="!Enter 4 digit Number", font=("Arial ", 7), fg='red')
+alert_label.grid(column=1, row=1)
+
+confirmation = Label(window, text="", font=("Arial ", 15))
+confirmation.grid(column=0, row=3)
+
+
+def clicked():
+    pin = int(input_pass.get())
     if len(str(pin)) == 4:
         if pin == password:
-            print("Correct Pin ....  ")
-            print("thank you")
-            break
+            confirmation.config(text='Correct Pin ...')
         else:
-            print("Incorrect Pin ....")
-            print(f"{chances} Chances Remaining ..")
-            chances -= 1
+            confirmation.config(text='"Incorrect Pin ...."')
     else:
-        print("Minimum 4 characters")
+        alert_label.config(text='Minimum 4 Numbers')
+
+
+submit = Button(window, text='Enter', bg='white', fg='black', command=clicked)
+submit.grid(column=1, row=2)
+
+
+window.mainloop()
